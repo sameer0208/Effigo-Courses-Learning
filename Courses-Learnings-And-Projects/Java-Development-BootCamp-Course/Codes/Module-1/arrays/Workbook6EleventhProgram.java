f9aab5ea738f115b1bf4a5bbcc47bfadc75d3234
sameer0208 <josephferon0901@gmail.com>
2025-01-10 12:05:14 +0530

    arrays concept codes added

---
+++ Courses-Learnings-And-Projects/Java-Development-BootCamp-Course/Codes/Module-1/arrays/Workbook6EleventhProgram.java
@@ -0,0 +1,26 @@
+package arrays;
+
+public class Workbook6EleventhProgram {
+    public static void main(String[] args) {
+        double[] celsius = {12.5, 14.5, 17.0, 21.0, 23.0, 18.5, 20.0};
+        double[] fahrenheit = celsiusToFahrenheit(celsius);
+        printTemperatures(celsius, "Celsius");
+        printTemperatures(fahrenheit, "Fahrenheit");
+    }
+
+    public static double[] celsiusToFahrenheit(double[] celsius) {
+        double[] fahrenheit = new double[celsius.length];
+        for (int i = 0; i < celsius.length; i++) {
+            fahrenheit[i] = (celsius[i]/5 * 9)+32;
+        }
+        return fahrenheit;
+    }
+
+    public static void printTemperatures(double[] temp, String type) {
+        System.out.print(type + ": ");
+        for (int i = 0; i < temp.length; i++) {
+            System.out.print(temp[i] + " ");
+        }
+        System.out.print("\n");
+    }
+}
---
+++ Courses-Learnings-And-Projects/Java-Development-BootCamp-Course/Codes/Module-1/arrays/Workbook6ThirteenthProgram.java
@@ -0,0 +1,4 @@
+package arrays;
+
+public class Workbook6ThirteenthProgram {
+}
